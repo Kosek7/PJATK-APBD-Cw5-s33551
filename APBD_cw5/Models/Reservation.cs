fb07ed8b8a7b04c1922876d86ee669095a64ffb6
Dawid-Piotrowski-s33551 <s33551@pjwstk.edu.pl>
2026-04-15 21:05:48 +0200

    Add data models

---
+++ APBD_cw5/Models/Reservation.cs
@@ -0,0 +1,13 @@
+﻿namespace APBD_cw5.Models;
+
+public class Reservation
+{
+    public int Id { get; set; }
+    public int RoomId { get; set; }
+    public string OrganizerName { get; set; }
+    public string Topic { get; set; }
+    public DateTime Date { get; set; }
+    public TimeSpan StartTime { get; set; }
+    public TimeSpan EndTime { get; set; }
+    public ReservationStatus Status { get; set; }
+}
---
+++ APBD_cw5/Models/ReservationStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace APBD_cw5.Models;
+
+public enum ReservationStatus
+{
+    Planned,
+    Confirmed,
+    Cancelled
+}
