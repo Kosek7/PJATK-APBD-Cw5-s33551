205f217e5990bb58398569090ec8549e8315c022
Dawid-Piotrowski-s33551 <s33551@pjwstk.edu.pl>
2026-04-15 21:18:40 +0200

    Create controllers and data lists

---
+++ APBD_cw5/Controllers/ReservationsController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using APBD_cw5.Models;
+
+namespace APBD_cw5.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ReservationsController : ControllerBase
+{
+    private static List<Reservation> _reservations = new()
+    {
+        new Reservation
+        {
+            Id = 1,
+            RoomId = 1,
+            OrganizerName = "Jan Kowalski",
+            Topic = "Spotkanie zespołu",
+            Date = new DateTime(2025, 5, 10),
+            StartTime = new TimeSpan(10, 0, 0),
+            EndTime = new TimeSpan(11, 0, 0),
+            Status = ReservationStatus.Planned
+        },
+        new Reservation
+        {
+            Id = 2,
+            RoomId = 2,
+            OrganizerName = "Anna Nowak",
+            Topic = "Prezentacja",
+            Date = new DateTime(2025, 5, 11),
+            StartTime = new TimeSpan(12, 0, 0),
+            EndTime = new TimeSpan(13, 30, 0),
+            Status = ReservationStatus.Confirmed
+        },
+        new Reservation
+        {
+            Id = 3,
+            RoomId = 3,
+            OrganizerName = "Piotr Wiśniewski",
+            Topic = "Szkolenie",
+            Date = new DateTime(2025, 5, 12),
+            StartTime = new TimeSpan(9, 0, 0),
+            EndTime = new TimeSpan(12, 0, 0),
+            Status = ReservationStatus.Planned
+        },
+        new Reservation
+        {
+            Id = 4,
+            RoomId = 1,
+            OrganizerName = "Katarzyna Zielińska",
+            Topic = "Warsztaty",
+            Date = new DateTime(2025, 5, 13),
+            StartTime = new TimeSpan(14, 0, 0),
+            EndTime = new TimeSpan(16, 0, 0),
+            Status = ReservationStatus.Cancelled
+        },
+        new Reservation
+        {
+            Id = 5,
+            RoomId = 4,
+            OrganizerName = "Tomasz Lewandowski",
+            Topic = "Rekrutacja",
+            Date = new DateTime(2025, 5, 14),
+            StartTime = new TimeSpan(11, 0, 0),
+            EndTime = new TimeSpan(12, 0, 0),
+            Status = ReservationStatus.Confirmed
+        }
+    };
+}
---
+++ APBD_cw5/Controllers/RoomsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using APBD_cw5.Models;
+
+namespace APBD_cw5.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RoomsController : ControllerBase
+{
+    private static List<Room> _rooms = new()
+    {
+        new Room { Id = 1, Name = "A101", BuildingCode = "A", Floor = 1, Capacity = 20, HasProjector = true, IsActive = true },
+        new Room { Id = 2, Name = "A102", BuildingCode = "A", Floor = 1, Capacity = 15, HasProjector = false, IsActive = true },
+        new Room { Id = 3, Name = "B201", BuildingCode = "B", Floor = 2, Capacity = 30, HasProjector = true, IsActive = true },
+        new Room { Id = 4, Name = "C301", BuildingCode = "C", Floor = 3, Capacity = 10, HasProjector = false, IsActive = false },
+        new Room { Id = 5, Name = "D401", BuildingCode = "D", Floor = 4, Capacity = 50, HasProjector = true, IsActive = true }
+    };
+}
